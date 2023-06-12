66bfabd1e91c81cb369f7db4e5d1f7850bd355d2
PravindaPerera <pravinda.joes@gmail.com>
2023-06-12 16:24:29 -0500

    renaming scripts files to better align with the applications

---
+++ App2.js
@@ -0,0 +1,147 @@
+const fs = require('fs');
+
+console.log('Start...');
+function generateYesValues() {
+    return Math.floor(Math.random() * (100 - 80 + 1)) + 80;
+}
+
+function generateNoValues() {
+    return Math.floor(Math.random() * (20 - 1 + 1)) + 1;
+}
+
+function generateShortValues() {
+    return Math.floor(Math.random() * (5 - 1 + 1)) + 1;
+}
+
+function generateLongValues() {
+    return Math.floor(Math.random() * (6 - 12 + 1)) + 10;
+}
+
+function generateVeryLongValues() {
+    return Math.floor(Math.random() * (13 - 20 + 1)) + 20;
+}
+
+
+const data = [];
+// generating Impulsive Buyer values
+for (let i = 0; i < 266; i++) {
+    const row = [];
+    for (let j = 1; j < 6; j++) {
+        switch (j) {
+            case 1:
+                row.push(generateNoValues());
+                break;
+            case 2:
+                row.push(generateNoValues());
+                break;
+            case 3:
+                row.push(generateYesValues());
+                break;
+            case 4:
+                row.push(generateYesValues());
+                break;
+            case 5:
+                row.push(generateShortValues());
+                break;
+            default:
+                console.log('Loop Error - generating Impulsive Buyer values');
+        }
+    }
+    data.push(row);
+}
+// generating Browsing Customer values
+for (let i = 0; i < 331; i++) {
+    const row = [];
+    for (let j = 1; j < 6; j++) {
+        switch (j) {
+            case 1:
+                row.push(generateYesValues());
+                break;
+            case 2:
+                row.push(generateYesValues());
+                break;
+            case 3:
+                row.push(generateYesValues());
+                break;
+            case 4:
+                row.push(generateYesValues());
+                break;
+            case 5:
+                row.push(generateVeryLongValues());
+                break;
+            default:
+                console.log('Loop Error - generating Browsing Customer values');
+        }
+    }
+    data.push(row);
+}
+
+// generating Loyal Customer values
+for (let i = 0; i < 179; i++) {
+    const row = [];
+    for (let j = 1; j < 6; j++) {
+        switch (j) {
+            case 1:
+                row.push(generateNoValues());
+                break;
+            case 2:
+                row.push(generateYesValues());
+                break;
+            case 3:
+                row.push(generateYesValues());
+                break;
+            case 4:
+                row.push(generateYesValues());
+                break;
+            case 5:
+                row.push(generateShortValues());
+                break;
+            default:
+                console.log('Loop Error - generating Loyal Customer values');
+        }
+    }
+    data.push(row);
+}
+
+// generating Bargain Hunter values
+for (let i = 0; i < 293; i++) {
+    const row = [];
+    for (let j = 1; j < 6; j++) {
+        switch (j) {
+            case 1:
+                row.push(generateYesValues());
+                break;
+            case 2:
+                row.push(generateYesValues());
+                break;
+            case 3:
+                row.push(generateNoValues());
+                break;
+            case 4:
+                row.push(generateYesValues());
+                break;
+            case 5:
+                row.push(generateLongValues());
+                break;
+            default:
+                console.log('Loop Error - generating Bargain Hunter values');
+        }
+    }
+    data.push(row);
+}
+
+
+const filename = 'data.csv';
+
+const csvData = data.map(row => row.join(',')); // Convert data rows to CSV format
+
+const csvContent = csvData.join('\n'); // Join rows with newline characters
+
+fs.writeFile(filename, csvContent, 'utf8', (err) => {
+    if (err) {
+        console.error('Error writing to CSV file:', err);
+        return;
+    }
+
+    console.log(`Data has been written to ${filename} successfully.`);
+});
